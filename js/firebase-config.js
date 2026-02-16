d027febc89991baaf6237193340e421a966247b8
msudkr713 <msudkr@gmail.com>
2026-02-15 20:48:24 -0500

    removed the database

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,27 @@
+// Firebase configuration disabled
+// To re-enable: Set up a new Firebase project and uncomment the code below
+
+/*
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUE5O0zVK36X3xnW8VVrSwoa48ZqbHN94",
-    authDomain: "silverhornetsdatabase.firebaseapp.com",
-    projectId: "silverhornetsdatabase",
-    storageBucket: "silverhornetsdatabase.firebasestorage.app",
-    messagingSenderId: "34196353442",
-    appId: "1:34196353442:web:f949b998821b8ffb8b82fb",
-    measurementId: "G-KK1SYKBF1F"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
+*/
+
+// Placeholder exports to prevent errors
+export const firebaseApp = null;
+export const auth = null;
+export const db = null;
